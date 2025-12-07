75e14e3b55ec773125d4ed7a03b5873289bacd86
Rhenan Dias <diasrhenan@gmail.com>
2025-12-07 09:50:58 -0300

    Adicionando solução final do dia 6

--- 2025/day06/02.ts
+++ 2025/day06/02.ts
@@ -19,12 +19,61 @@ const rl = readline.createInterface({
   crlfDelay: Infinity,
 });
 
-let result = 0;
+const exercises: string[][] = [];
+const operations: string[] = [];
 
 for await (const line of rl) {
   if (line.length === 0) continue;
 
-  console.log(line);
+  const elements = line.split("");
+
+  elements.forEach((el, idx) => {
+    if (["*", "+"].includes(el)) {
+      operations.push(el);
+      return;
+    }
+
+    if (exercises.length - 1 >= idx) {
+      exercises[idx] = [...exercises[idx], el];
+    } else {
+      exercises.push([el]);
+    }
+  });
 }
+
+const cleaned = exercises.map((el) => el.join("")).map((el) => Number(el));
+
+const grouped: number[][] = [];
+
+let group: number[] = [];
+
+for (let i = 0; i < cleaned.length; i++) {
+  const el = cleaned[i];
+
+  if (el !== 0 && i < cleaned.length) {
+    group.push(el);
+  }
+
+  if (el == 0 || i === cleaned.length - 1) {
+    grouped.push(group);
+    group = [];
+  }
+}
+
+const results: number[] = [];
+
+grouped.forEach((values, idx) => {
+  const operation = operations[idx];
+
+  if (operation === "*") {
+    results.push(
+      values.reduce((a, v) => a * Number(v), Number(values.shift()))
+    );
+  } else if (operation === "+") {
+    results.push(values.reduce((a, v) => a + Number(v), 0));
+  }
+});
+
+let result = results.reduce((a, v) => a + v, 0);
 
 console.log("Final Result:", result);
